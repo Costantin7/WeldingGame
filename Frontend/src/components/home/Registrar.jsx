0f8888c87ecd27a1fd0e99ce29e63ffedf52acb3
Costantin_ <gabrielfc08@yahoo.com.br>
2025-06-30 11:47:43 -0300

    Menu registrar usuario

--- Frontend/src/components/home/Registrar.jsx
+++ Frontend/src/components/home/Registrar.jsx
@@ -2,6 +2,17 @@ import { useEffect, useState } from "react";
 
 function Resgistrar(props) {
   const [close, setClose] = useState(false);
+  const [nome, setNome] = useState("Insira seu nome");
+  const [apelido, setApelido] = useState("Insira seu apelido");
+  const [email, setEmail] = useState("Insira seu nome");
+  const [senha, setSenha] = useState("Insira sua senha");
+  const [confirmarsenha, setConfirmarsenha] = useState(
+    "Insira seu confirmarsenha"
+  );
+  const [instituicao, setInstituicao] = useState("Insira sua instituicao");
+  const [pais, setPais] = useState("Insira seu pais");
+  const [profissao, setProfissao] = useState("Insira sua profissao");
+  const [escolaridada, setEscolaridada] = useState("Insira sua escolaridada");
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,8 +31,121 @@ function Resgistrar(props) {
         className="fixed inset-0 backdrop-blur-sm z-40 "
       ></div>
 
-      <div className="flex flex-col justify-center items-center fixed top-20 right-80 z-50 bg-white border border-black rounded-xl shadow-lg p-6 space-y-3 w-150 h-120 font-serif">
-        <p className="font-bold my-10">Registar usuário</p>
+      <div className="relative fixed z-50 left-60 bottom-70">
+        <div className="absolute flex flex-col justify-center bg-white border border-black rounded-xl shadow-lg p-6 w-150 h-180 font-serif ">
+          <p className="font-semibold !self-center text-xl	">Registro</p>
+          <div className="bg-gray-300 w-full h-1 mb-6 "></div>
+
+          <p className="font-semibold text-left">Nome: </p>
+          <input
+            className="border w-full mb-5"
+            type="text"
+            value={nome}
+            onChange={(e) => {
+              setNome(e);
+            }}
+          />
+          <div>
+            <p className="font-semibold	text-left">Apelido: </p>
+            <input
+              className="border w-full mb-5"
+              type="text"
+              value={apelido}
+              onChange={(e) => {
+                setApelido(e);
+              }}
+            />
+          </div>
+          <div>
+            <p className="font-semibold	text-left">E-mail: </p>
+            <input
+              className="border w-full mb-5"
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e);
+              }}
+            />
+          </div>
+          <div>
+            <p className="font-semibold	text-left">Senha: </p>
+            <input
+              className="border w-full mb-5"
+              type="text"
+              value={senha}
+              onChange={(e) => {
+                setSenha(e);
+              }}
+            />
+          </div>
+
+          <div>
+            <p className="font-semibold	text-left">Confirmar Senha: </p>
+            <input
+              className="border w-full mb-5"
+              type="text"
+              value={confirmarsenha}
+              onChange={(e) => {
+                setConfirmarsenha(e);
+              }}
+            />
+          </div>
+
+          <div className="flex flex-row ">
+            <div className="flex flex-col mr-25">
+              <div>
+                <p className="font-semibold	text-left">Instituição: </p>
+                <input
+                  className="border w-full mb-5"
+                  type="text"
+                  value={instituicao}
+                  onChange={(e) => {
+                    setInstituicao(e);
+                  }}
+                />
+              </div>
+              <div>
+                <p className="font-semibold	text-left">Pais: </p>
+                <input
+                  className="border w-full mb-5"
+                  type="text"
+                  value={pais}
+                  onChange={(e) => {
+                    setPais(e);
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col ml-25">
+              <div>
+                <p className="font-semibold	text-left">Profissao: </p>
+                <input
+                  className="border w-full mb-5"
+                  type="text"
+                  value={profissao}
+                  onChange={(e) => {
+                    setProfissao(e);
+                  }}
+                />
+              </div>
+              <div>
+                <p className="font-semibold	text-left">Escolaridade: </p>
+                <input
+                  className="border w-full mb-5"
+                  type="text"
+                  value={escolaridada}
+                  onChange={(e) => {
+                    setEscolaridada(e);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <button className="!bg-blue-400 justify-self-center">
+            Cadastrar
+          </button>
+        </div>
       </div>
     </div>
   );
