a0e3afbde90358264bf6ba03ddd73267f5fd6062
Costantin_ <gabrielfc08@yahoo.com.br>
2025-07-07 11:12:55 -0300

    Botão deslizante estilizado

--- Frontend/src/components/config convidado/botão deslizante.jsx
+++ Frontend/src/components/config convidado/botão deslizante.jsx
@@ -10,7 +10,7 @@ function BotaoDeslizante(props) {
         {!props.parameter ? (
           <div>
             <div className=" absolute bottom-4 right-3  h-[21px] w-[50px] bg-gray-400 rounded-xl"></div>
-            <div className=" absolute bottom-4 right-3 h-[21px] w-[21px] bg-blue-600 rounded-xl"></div>
+            <div className=" absolute bottom-4 right-3 h-[21px] w-[21px] bg-gray-800 rounded-xl"></div>
           </div>
         ) : (
           <div>
