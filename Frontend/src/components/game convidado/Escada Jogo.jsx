1e5c1c90caad8f9bf110a925fe73f378ce7536d5
Costantin_ <gabrielfc08@yahoo.com.br>
2025-07-09 08:34:30 -0300

    Tela erro semi-estilizada

--- Frontend/src/components/game convidado/Escada Jogo.jsx
+++ Frontend/src/components/game convidado/Escada Jogo.jsx
@@ -1,43 +1,49 @@
 import React from "react";
 import Soldadores from "./Soldadores";
 
+function Lugarnaescada(props) {
+  var valy0 = -props.level * 20 + 275;
+  var valx0 = -props.level * 20 + 1040;
+  var valy = -props.level * 24 + 260;
+  var valx = -props.level * 44 + 920;
 
-function Lugarnaescada(props)
-{
-
-    var valy0=-props.level*20+275;
-    var valx0=-props.level*20+1040;
-    var valy=-props.level*24+260;
-    var valx=-props.level*44+920;
-
-    if(props.level==0)
-    {
-        return(
-            <div className="relative z-50 w-[120px]" style={{ top: `${valy0}px`, left: `${valx0}px` }}> 
-                <img src={`./src/img/Soldadores/Nivel${props.level}.png`}  alt={`nivel ${props.level}`}  />
-            </div>
-        );
-    }
-    else
-    {
-        return(
-            <div className="relative z-50 w-[220px]" style={{ top: `${valy}px`, left: `${valx}px` }}> 
-                <img src={`./src/img/Soldadores/Nivel${props.level}.png`}  alt={`nivel ${props.level}`}  />
-            </div>
-        );
-    }
-}
-    
-    //${-props.level*10
-function EscadaJogo(props){
-    return(
-        <div>
-            <div className="w-[1200px]">
-                <img className="absolute " src={props.link} alt={props.nome} />
-                <Lugarnaescada level={props.nivel}/> 
-            </div>
-        </div>
+  if (props.level == 0) {
+    return (
+      <div
+        className="relative z-45 w-[120px]"
+        style={{ top: `${valy0}px`, left: `${valx0}px` }}
+      >
+        <img
+          src={`./src/img/Soldadores/Nivel${props.level}.png`}
+          alt={`nivel ${props.level}`}
+        />
+      </div>
     );
+  } else {
+    return (
+      <div
+        className="relative z-30 w-[220px]"
+        style={{ top: `${valy}px`, left: `${valx}px` }}
+      >
+        <img
+          src={`./src/img/Soldadores/Nivel${props.level}.png`}
+          alt={`nivel ${props.level}`}
+        />
+      </div>
+    );
+  }
+}
+
+//${-props.level*10
+function EscadaJogo(props) {
+  return (
+    <div>
+      <div className="w-[1200px]">
+        <img className="absolute " src={props.link} alt={props.nome} />
+        <Lugarnaescada level={props.nivel} />
+      </div>
+    </div>
+  );
 }
 
 export default EscadaJogo;
--- Frontend/src/components/game convidado/Tela_Erro.jsx
+++ Frontend/src/components/game convidado/Tela_Erro.jsx
@@ -20,7 +20,7 @@ function TelaErro(props) {
         className="fixed inset-0 backdrop-blur-sm z-40 "
       ></div>
 
-      <div className="fixed top-20 right-80 z-50 bg-white border border-black rounded-xl shadow-lg p-6 space-y-3 w-150 h-120 font-serif">
+      <div className="relative bottom-100 right-30 z-1000 bg-white border border-black rounded-xl shadow-lg p-6 space-y-3 w-150 h-80 font-serif">
         <div
           className="place-self-end w-8 flex justify-center px-4 py-1 cursor-pointer center-items"
           onClick={() => {
@@ -29,7 +29,17 @@ function TelaErro(props) {
         >
           <p>❌</p>
         </div>
-        <p>Errou!</p>
+        <p className="font-bold text-xl">Você errou!</p>
+        <div className="bg-gray-400 h-1"></div>
+        <div className="my-2">
+          <p>Nível atingido: *importar nivel</p>
+          <p>Resposta correta: *importar resposta</p>
+          <p>Explicação: *importar explicações </p>
+        </div>
+        <div className="flex flex-row justify-center mx-3 my-8">
+          <button className=" mx-2 !bg-blue-500"> Reiniciar </button>
+          <button className="mx-2 !bg-red-500"> Sair </button>
+        </div>
       </div>
     </div>
   );
