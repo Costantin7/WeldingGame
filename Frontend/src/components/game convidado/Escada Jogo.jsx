be3597b6f9ab748f9d950cc967b9060103287f9c
Descktop3 <gabrielfc08@yahoo.com.br>
2025-09-11 12:02:25 -0300

    medinhos base

--- Frontend/src/components/game convidado/Escada Jogo.jsx
+++ Frontend/src/components/game convidado/Escada Jogo.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import Soldadores from "./Soldadores";
 import NivelSoldador from "./NivelSoldador";
 
+function Medinhos(props) {
+  const elementos = [];
+  for (let i = 20; i > props.level; i--) {
+    const valmy = -i * 100 + 2000;
+    const valmx = -i * 14 + 2300;
+
+    elementos.push(
+      <div
+        key={i}
+        className="absolute z-20 w-[100px]"
+        style={{ top: `${valmy}px`, left: `${valmx}px` }}
+      >
+        <img src={`./src/img/medinhos/nuvem${i}.png`} alt={`medinho ${i}`} />
+      </div>
+    );
+  }
+
+  return elementos;
+}
+
 function Lugarnaescada(props) {
   var valy0 = -props.level * 20 + 275;
   var valx0 = -props.level * 20 + 1040;
@@ -41,6 +61,7 @@ function EscadaJogo(props) {
       <div className="w-full relative">
         <img className="absolute" src={props.link} alt={props.nome} />
         <Lugarnaescada level={props.nivel} />
+        <Medinhos level={props.nivel} />
         <div className="flex absolute top-10 left-280 z-10">
           <NivelSoldador nivel={props.nivel} />
         </div>
