c079e57cb7fbaf509684f45022f2df2531e89109
Descktop3 <gabrielfc08@yahoo.com.br>
2025-09-18 15:10:24 -0300

    tela pelo tamanho da tela

--- Frontend/src/components/home/Empresas.jsx
+++ Frontend/src/components/home/Empresas.jsx
@@ -1,9 +1,13 @@
-function Empresas(props){
-    return(
-        <a target="_blank" rel="noopener noreferrer" href={props.site} > 
-            <img className="h-[50px] grayscale object-contain" alt={props.nome} src={props.imagem} /> 
-        </a>
-    )
+function Empresas(props) {
+  return (
+    <a target="_blank" rel="noopener noreferrer" href={props.site}>
+      <img
+        className=" h-[25px] sm:h-[35px] md:h-[45px] lg:h-[65px] grayscale "
+        alt={props.nome}
+        src={props.imagem}
+      />
+    </a>
+  );
 }
 
 export default Empresas;
